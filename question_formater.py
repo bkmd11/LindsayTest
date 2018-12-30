002a7fd912f5816e7728d07654d2dd749e48520e
bkmd11 <bkendall11@gmail.com>
2018-12-30 14:18:03 -0500

    swapped key and values so i can actually do this the way i wanted

--- question_formater.py
+++ question_formater.py
@@ -3,21 +3,7 @@
 import pprint
 """ the goal of this file is to format a set of strings into a list
 of dictionaries of question/answer groups.
-
-I need to figure out how to extract questions from a word file, and seperate
-the multiple choice answers into a seperate dictionary
-
-example format of one key value pair:
-
-Level_one_multi_choice = {'What is a hypothesis?':{'A guess': False,
-                    'A prediction about the outcome of an experiment based on prior knowledge': True,
-                    'The actual outcome of an experiment': False,
-                    'A prediction about the outcome of an experiment without background knowledge': False}}
-
 """
-
-#cant use value to call key!
-#need to rewrite so the answer option is the value, and an identifier is the key
 
 dictionary = {}
 question = 'spam'
